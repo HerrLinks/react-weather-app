57d13f29450edc971220674a9b8abc0a464125a1
HerrLinks <atti.szucs85@gmail.com>
2023-02-22 11:44:56 +0100

    maximum population defined

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a8238e9da7msh556c36b98f4f6fap1a9815jsn47f0bd5d1ec0',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
